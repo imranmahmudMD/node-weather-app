83b5acbf4e2e2b4c5d39605db3f5a65367eb8a90
Imran Mahmud <imran@mail.harvard.edu>
2021-02-11 16:19:41 +0000

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client-side javascript file is loaded!')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')// hashtag indicates targeting an id
+const messageTwo = document.querySelector('#message-2')// hashtag indicates targeting an id
+
+
+// note search.value takes the input value from search
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() // stops the browser from refreshing which is the default behaviour
+    
+    const location = search.value
+    console.log(location)
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+        if(data.error) {
+        messageOne.textContent = ''
+        messageTwo.textContent = data.error
+        } else {
+            messageOne.textContent = ('The forecast for ' + data.location + ' is ' + data.forecast)
+            messageTwo.textContent = ''
+            }
+        })
+    })
+})
+
+// console.log(data.location)
+//             console.log(data.forecast)
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require("postman-request")
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ca7abdaac6d121fbed6c78d594d91231&query=' + latitude + "," + longitude + '&units=f'
+    request({ url, json: true}, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to the weather service', undefined)
+        } else if(body.error) {
+            callback('Unable to find that location?', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' today. The temperature is ' + body.current.temperature + ' degrees, and the wind speed is ' + body.current.wind_speed + ' miles per hour')
+            }
+        })
+    }
+
+module.exports = forecast
